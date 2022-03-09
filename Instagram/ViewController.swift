37b2c930c153e9f6a97d92c271745fa6c5c69270
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-03-09 14:55:55 -0500

    Initial commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Vinojen Gengatharan on 2022-03-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
