5de32084003b832834257a0995ccf8113d83ea20
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2022-03-10 18:31:42 -0500

    Created custom error that will be used with Firebase Authentication

---
+++ Instagram/Resources/Managers/AuthError.swift
@@ -0,0 +1,14 @@
+//
+//  AuthError.swift
+//  Instagram
+//
+//  Created by Vinojen Gengatharan on 2022-03-10.
+//
+
+import Foundation
+
+enum AuthError : Error
+{
+    case signInError
+    case signOutError
+}
